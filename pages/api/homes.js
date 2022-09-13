d711ec9922c0c83961f5e854d26fbac66177a127
Josué Munganga <jmunganga039@gmail.com>
2022-09-13 13:34:29 +0200

    built the create home listing api route

--- pages/api/homes.js
+++ pages/api/homes.js
@@ -1,7 +1,28 @@
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient()
+
 export default async function handler(req, res) {
   // Create new home
   if (req.method === 'POST') {
-    // TODO
+    try {
+      const { image, title, description, price, guests, beds, baths } = req.body;
+      const home =  await prisma.home.create({
+        data: {
+          image,
+          title,
+          description,
+          price,
+          guests,
+          beds,
+          baths
+        }
+      })
+
+      res.status(200).json(home);
+    } catch (err) {
+      res.status(500).json({ message: 'Something went wrong' });
+    }
   }
   // HTTP method not supported!
   else {
